5561082e45840d320741d8754636188cf2bf9ab3
ntrung176 <lynguyennhattrung1706@gmail.com>
2026-02-17 11:04:25 +0700

    fix: update base path to match repository name

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/bao-appdemo/', // Change this to your GitHub repository name
+    base: '/script-creator/', // GitHub repository name
     server: {
         port: 3000,
         open: true
